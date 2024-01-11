1e1e4bd45d41a7b7684f1ddc153860b1862795c4
bagaspoetra <bagaspoetra04884@gmail.com>
2024-01-11 09:56:18 +0700

    prjectuas

---
+++ APLIKASI BARANG/backup/unit5.pas
@@ -0,0 +1,26 @@
+unit Unit5;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+  TForm5 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form5: TForm5;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ APLIKASI BARANG/unit5.pas
@@ -0,0 +1,30 @@
+unit Unit5;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm5 }
+
+  TForm5 = class(TForm)
+    Label1: TLabel;
+  private
+
+  public
+
+  end;
+
+var
+  Form5: TForm5;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
